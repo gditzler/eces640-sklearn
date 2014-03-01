304af2a7deeb468f2ac4e90679b1033ef95f2ba6
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-01 16:56:50 -0500

    added some sklearn stuff

--- src/demo-mds.py
+++ src/demo-mds.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python 
 import utils
 import bmu
+import pickle 
+from sklearn import manifold
 
 # what phenotypes do we want to use?
 phenotype = "COMMON_SAMPLE_SITE"
@@ -16,5 +18,15 @@ meta_data = bmu.load_map(map_fp)
 phenotype_ids = utils.return_phenotypes(sample_ids, meta_data, phenotype)
 
 # compute the distance matrix using the hellinger divergence
-print "Computing distances..."
-dist_mat = utils.compute_distance_matrix(data)
+print "Loading distances..."
+dist_mat = pickle.load(open("../data/distances_study_550.pkl", "rb"))
+
+mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed, 
+    dissimilarity="precomputed", n_jobs=1)
+pos = mds.fit(similarities).embedding_
+
+
+
+plt.scatter(pos[:, 0], pos[:, 1], s=20, c='g')
+
+
