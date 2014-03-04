4809677158296704bfa0295389fa4d16e26f6a68
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-04 16:16:40 -0500

    initial commit

---
+++ src/demo-manifold.py
@@ -0,0 +1,26 @@
+from pylab import *
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import manifold, datasets
+
+"""
+Based on scikits.learn example at
+http://scikit-learn.org/stable/auto_examples/manifold/plot_swissroll.html
+"""
+
+# load Swiss roll dataset
+X, color = datasets.samples_generator.make_swiss_roll(n_samples=1500)
+
+# run Isomap on the points in X with 2 dim output
+n_neighbors = 10
+Y = manifold.Isomap(n_neighbors, 2).fit_transform(X)
+
+# 3D plot
+fig = figure()
+ax = fig.gca(projection='3d')
+ax.scatter(X[:,0], X[:,1], X[:,2], c=color)
+
+# 2D projection
+figure()
+scatter(Y[:,0], Y[:,1], c=color)
+
+show()
