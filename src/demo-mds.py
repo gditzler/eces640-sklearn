a1148ae197ef50120ab7cf9ed74f098a4424a095
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-04 15:58:58 -0500

    functional

--- src/demo-mds.py
+++ src/demo-mds.py
@@ -19,22 +19,26 @@ data, sample_ids, otus_names = bmu.load_biom(biom_fp)
 meta_data = bmu.load_map(map_fp)
 phenotype_ids = utils.return_phenotypes(sample_ids, meta_data, phenotype)
 
+i_subset = numpy.where(phenotype_ids=="gut")
+
 # compute the distance matrix using the hellinger divergence
 print "Loading distances..."
-dist_mat = pickle.load(open("../data/distances_study_550.pkl", "rb"))
-
+dist_mat = pickle.load(open("../data/distances_study_500.pkl", "rb"))
+ 
 # perform mutli-dimensional scaling
-mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed, 
-    dissimilarity="precomputed", n_jobs=1)
-pos = mds.fit(similarities).embedding_
+print "Running MDS..."
+mds = manifold.MDS(n_components=2, max_iter=300, eps=1e-9, random_state=1, 
+     dissimilarity="precomputed", n_jobs=4)
+pos = mds.fit(dist_mat).embedding_
 
 # create a 2D plot of the results
+print "Making plots..."
 fig = plt.figure(1)
 colors = ['r','b','k','m']
-
+ 
 for i,site in enumerate(numpy.unique(phenotype_ids)):
-  subset = np.where(phenotype_ids == site)
-  plt.scatter(pos[subset, 0], pos[subset, 1], s=20, c=color[i])
+  subset = numpy.where(phenotype_ids == site)
+  plt.scatter(pos[subset, 0], pos[subset, 1], s=20, c=colors[i])
 
 plt.legend(tuple(numpy.unique(phenotype_ids)), loc='best')
 plt.show()
